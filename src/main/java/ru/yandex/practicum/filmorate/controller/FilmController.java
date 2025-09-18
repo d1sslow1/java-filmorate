275669254219dbfc0632516747f663ea5cf36f7c
d1sslow1 <artemdanielyan2006@yandex.ru>
2025-09-18 13:38:50 +0700

    Проработал все ТЗ и попрактиковался в постмане

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -2,11 +2,10 @@ package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
+import jakarta.validation.Valid;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.FilmService;
 
-import java.time.LocalDate;
 import java.util.Collection;
 import java.util.List;
 
@@ -32,27 +31,14 @@ public class FilmController {
     }
 
     @PostMapping
-    public Film create(@RequestBody Film film) {
-        if (film.getName() == null || film.getName().isBlank()) {
-            throw new ValidationException("Название фильма не может быть пустым");
-        }
-        if (film.getDescription() != null && film.getDescription().length() > 200) {
-            throw new ValidationException("Описание фильма не может быть длиннее 200 символов");
-        }
-        if (film.getDuration() <= 0) {
-            throw new ValidationException("Длительность фильма должны быть положительным числом");
-        }
-        if (film.getReleaseDate() != null && film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
-            throw new ValidationException("Дата релиза не может быть раньше 28 декабря 1895 года");
-        }
-
+    public Film create(@Valid @RequestBody Film film) {
         Film created = filmService.createFilm(film);
         log.info("Добавлен фильм: {}", created);
         return created;
     }
 
     @PutMapping
-    public Film update(@RequestBody Film film) {
+    public Film update(@Valid @RequestBody Film film) {
         Film updated = filmService.updateFilm(film);
         log.info("Обновлён фильм: {}", updated);
         return updated;
--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,7 +1,7 @@
 package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
-
+import jakarta.validation.constraints.*;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
@@ -9,9 +9,18 @@ import java.util.Set;
 @Data
 public class Film {
     private Integer id;
+
+    @NotBlank(message = "Название фильма не может быть пустым")
     private String name;
+
+    @Size(max = 200, message = "Описание фильма не может быть длиннее 200 символов")
     private String description;
+
+    @NotNull(message = "Дата релиза обязательна")
+    @PastOrPresent(message = "Дата релиза не может быть раньше 28 декабря 1895 года")
     private LocalDate releaseDate;
+
+    @Positive(message = "Длительность фильма должна быть положительным числом")
     private Integer duration;
 
     private Set<Integer> likes = new HashSet<>();
