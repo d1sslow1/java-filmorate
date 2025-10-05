965b32215adf576661ecefa5da05cbac8180d41f
d1sslow1 <artemdanielyan2006@yandex.ru>
2025-10-05 21:35:35 +0700

    Доделал приложение для управления  фильмами и пользователями с системой лайков и друзей

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -7,8 +7,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import ru.yandex.practicum.filmorate.model.Film;
-import java.time.LocalDate;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -24,14 +22,11 @@ class FilmControllerTest {
 
     @Test
     void createValidFilm() throws Exception {
-        Film film = new Film();
-        film.setName("Film");
-        film.setReleaseDate(LocalDate.of(2000, 1, 1));
-        film.setDuration(120);
+        String filmJson = "{\"name\":\"Film\",\"releaseDate\":\"2000-01-01\",\"duration\":120,\"mpa\":{\"id\":1}}";
 
         mockMvc.perform(post("/films")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(film)))
+                        .content(filmJson))
                 .andExpect(status().isOk());
     }
 }
